bbd3c56855421294dd3c9a6d8872b1bef3766a31
Shaharyar Ahmed <workwithraw@gmail.com>
2022-08-30 15:27:02 +0500

    Create Article feature added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' , to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
