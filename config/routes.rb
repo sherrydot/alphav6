a370916ca6147cec1dffcfaf9167715a5a9582d2
Shaharyar Ahmed <workwithraw@gmail.com>
2022-08-30 14:14:29 +0500

    Aricles lisitng feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' , to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end
