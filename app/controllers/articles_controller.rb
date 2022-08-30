686ad42dc04bdd28c5eb215eb4248af96e3de625
Shaharyar Ahmed <workwithraw@gmail.com>
2022-08-30 19:18:57 +0500

    Article parameters check added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,14 +8,18 @@ class ArticlesController < ApplicationController
   end
   
   def new
-  
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
     
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Created Successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 end
