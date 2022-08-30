95fcad9a09551fd811fb3ae4c15173c5c0e9b7ee
Shaharyar Ahmed <workwithraw@gmail.com>
2022-08-30 13:47:44 +0500

    retrieve articles and show on screen

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_08_29_115150) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
