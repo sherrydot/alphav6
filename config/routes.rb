decc3a0367e9a613963e8aa8e90dfdf5d0b7136f
Shaharyar Ahmed <workwithraw@gmail.com>
2022-09-09 15:31:37 +0500

    New User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'about' , to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
