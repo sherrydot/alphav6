f7090f59453114cf4b2d15e87da8359074294422
Shaharyar Ahmed <workwithraw@gmail.com>
2022-09-12 15:53:55 +0500

    Add Edit User Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,26 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User has been updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
