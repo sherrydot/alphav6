9d675f8959a4242152a26a8f6a4d7b5f7949af96
Shaharyar Ahmed <workwithraw@gmail.com>
2022-09-13 17:36:29 +0500

    Login and Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Successfully Logged In"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your entered credentials"
+      render 'new'
+    end
+
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
-  
+
 end
