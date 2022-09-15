dcdf529c3de01e1995f938c1ae1350d859820f61
Shaharyar Ahmed <workwithraw@gmail.com>
2022-09-15 15:46:08 +0500

    Delete user and associated articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
   before_save {self.email = email.downcase}
-  has_many :articles
+  # dependent destroy will delete articles associated to a user that has been deleted
+  has_many :articles, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   
