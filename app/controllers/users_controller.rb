5f192720cc3a5aa1e58526309c9bd2c57dc8d035
Shaharyar Ahmed <workwithraw@gmail.com>
2022-09-15 15:10:28 +0500

    Before action :require user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
 
   def new
     @user = User.new
