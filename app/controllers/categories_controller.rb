018b75abb374eabc3f1dfebb9bd16b6599c7b08b
Shaharyar Ahmed <workwithraw@gmail.com>
2022-09-16 17:02:09 +0500

    create category and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,29 @@
 class CategoriesController < ApplicationController
 
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)  
+    if @category.save
+      flash[:notice] = "Category created Successfully"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+  
   def index
   end
 
-  def new
-  end
-  
   def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
