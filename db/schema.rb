5a01faaa5980523d928792e75d9a657bd252dd50
Shaharyar Ahmed <workwithraw@gmail.com>
2022-09-20 15:50:05 +0500

    mant-to-many backend implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_095452) do
+ActiveRecord::Schema.define(version: 2022_09_20_103036) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
